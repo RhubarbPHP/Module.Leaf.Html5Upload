0e250d987da24385b62ba351b9f702f186870337
Luke Roantree <lroantree@gcdtech.com>
2016-08-12 10:27:57 +0100

    updates; now has a progress indicator, upload cancel button, and can display upload speed

---
+++ src/Html5FileUpload.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) 2016 RhubarbPHP.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+use Rhubarb\Leaf\Leaves\Leaf;
+
+/**
+ * Created by PhpStorm.
+ * User: Luke
+ * Date: 05/08/2016
+ * Time: 14:26
+ */
+class Html5FileUpload extends Leaf
+{
+
+    /**
+     * Returns the name of the standard view used for this leaf.
+     *
+     * @return string
+     */
+    protected function getViewClass()
+    {
+        // TODO: Implement getViewClass() method.
+    }
+
+    /**
+     * Should return a class that derives from LeafModel
+     *
+     * @return \Rhubarb\Leaf\Leaves\LeafModel
+     */
+    protected function createModel()
+    {
+        // TODO: Implement createModel() method.
+    }
+}
---
+++ src/Html5FileUploadModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luke
+ * Date: 08/08/2016
+ * Time: 11:46
+ */
+
+namespace Rhubarb\Leaf\Controls\Html5Upload;
+
+
+class Html5FileUploadModel
+{
+
+}
